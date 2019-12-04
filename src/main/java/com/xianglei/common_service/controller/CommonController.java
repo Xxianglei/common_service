fb2fb44dab9485bd22c6a8c8ee30bafcd1ea5870
向磊 <xianglei@sefonsoft.com>
2019-12-04 21:34:13 +0800

    update key

--- src/main/java/com/xianglei/common_service/controller/CommonController.java
+++ src/main/java/com/xianglei/common_service/controller/CommonController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -50,7 +51,7 @@ public class CommonController {
                         // token存入session session 存入redis
                         session.setAttribute("user_flowId", token);
                         baseJson.setMessage("登录成功");
-                        baseJson.setData(token);
+                        baseJson.setToken(token);
                         baseJson.setStatus(true);
                         baseJson.setCode(HttpStatus.OK.value());
                     } else {
