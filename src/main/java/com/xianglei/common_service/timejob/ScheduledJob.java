b123e1cf4e1367bf8daba5836e977d33be2da333
向磊 <xianglei@sefonsoft.com>
2019-12-08 17:34:06 +0800

    调度任务bug修复

--- src/main/java/com/xianglei/common_service/timejob/ScheduledJob.java
+++ src/main/java/com/xianglei/common_service/timejob/ScheduledJob.java
@@ -1,5 +1,6 @@
 package com.xianglei.common_service.timejob;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.xianglei.common_service.common.JwtUtils;
 import com.xianglei.common_service.common.Tools;
 import com.xianglei.common_service.common.utils.RedisUtil;
@@ -12,10 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Component
@@ -34,29 +35,36 @@ public class ScheduledJob {
      *
      * @Scheduled:设置定时任务 cron属性：cron表达式。定时任务触发是时间的一个字符串表达形式
      */
-    @Scheduled(cron = "0 0/30 * * * ?")
+    @Scheduled(cron = "0 0/10 * * * ?")
     public void scheduledMethod() throws Exception {
         logger.info("Scheduled定时清理非法在线用户触发，时间：{}", new Date());
         List<String> allUser = userMangerService.findAllUserNoPrama();
         // 获取在线用户
         List<String> isOnlineFlowId = allUser.stream().filter(m -> "1".equals(m)).collect(Collectors.toList());
         Set<String> keys = redisUtil.getKeys();
-        if (Tools.isEmpty(keys)&&!isOnlineFlowId.isEmpty()) {
+        if (Tools.isEmpty(keys) && !isOnlineFlowId.isEmpty()) {
             for (String flowId : isOnlineFlowId) {
                 logger.info("Redis缓存清空同步任务\n" +
                         "强制下线---->时间：{}", new Date());
                 userService.logout(flowId);
             }
         } else {
-            for (String key : keys) {
-                if (JwtUtils.verify(key)) {
-                    String flowId = JwtUtils.getFlowId(key);
-                    // 数据库中用户在线，但是token已经没有了，失效了，则强制下线
-                    if (!isOnlineFlowId.contains(flowId)) {
-                        logger.info("部分用户：{}token失效\n" +
-                                "强制下线---->时间：{}", flowId, new Date());
-                        userService.logout(flowId);
+            for (String OnlineflowId : isOnlineFlowId) {
+                boolean tag = false;
+                for (String key : keys) {
+                    if (JwtUtils.verify(key)) {
+                        String flowId = JwtUtils.getFlowId(key);
+                        if (OnlineflowId.equals(flowId)) {
+                            tag = true;
+                            break;
+                        }
                     }
+                }
+                if (!tag) {
+                    // 数据库中用户在线，但是token已经没有了，失效了，则强制下线
+                    logger.info("部分用户：{}token失效\n" +
+                            "强制下线---->时间：{}", OnlineflowId, new Date());
+                    userService.logout(OnlineflowId);
                 }
             }
         }
