851e3fe0aed3d18608f85f4b86ac5131c6fadcbf
xianglei <xianglei@sefonsoft.com>
2020-04-19 10:49:12 +0800

    .json 后缀

--- src/main/java/com/xianglei/common_service/CommonServiceApplication.java
+++ src/main/java/com/xianglei/common_service/CommonServiceApplication.java
@@ -3,9 +3,12 @@ package com.xianglei.common_service;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,18 +21,16 @@ public class CommonServiceApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(CommonServiceApplication.class, args);
     }
-    /*
-     * 设置匹配*.json后缀请求
+    /* * 设置匹配*.json后缀请求
      * @param dispatcherServlet
      * @return-该设置严格指定匹配后缀*.do或.action，但有风险 可能不生效
      *
-     *
+     **/
     @Bean
     public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
         ServletRegistrationBean<DispatcherServlet> servletServletRegistrationBean = new ServletRegistrationBean<>(dispatcherServlet);
         servletServletRegistrationBean.addUrlMappings("*.json");
         return servletServletRegistrationBean;
     }
-     */
 }
 
