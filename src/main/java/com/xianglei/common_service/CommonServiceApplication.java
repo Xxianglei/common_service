52c3d9e3d9dbd6ecb7b039871788d12144f523f1
xianglei <xianglei@sefonsoft.com>
2020-04-19 11:33:31 +0800

    update

--- src/main/java/com/xianglei/common_service/CommonServiceApplication.java
+++ src/main/java/com/xianglei/common_service/CommonServiceApplication.java
@@ -21,16 +21,16 @@ public class CommonServiceApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(CommonServiceApplication.class, args);
     }
-    /* * 设置匹配*.json后缀请求
+   /* *//* * 设置匹配*.json后缀请求
      * @param dispatcherServlet
      * @return-该设置严格指定匹配后缀*.do或.action，但有风险 可能不生效
      *
-     **/
+     **//*
     @Bean
     public ServletRegistrationBean servletRegistrationBean(DispatcherServlet dispatcherServlet) {
         ServletRegistrationBean<DispatcherServlet> servletServletRegistrationBean = new ServletRegistrationBean<>(dispatcherServlet);
         servletServletRegistrationBean.addUrlMappings("*.json");
         return servletServletRegistrationBean;
-    }
+    }*/
 }
 
